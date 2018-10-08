ed1008558c4399d4c4f9797c2d8d6a8822ad2a28
Gzmy <1370711580@qq.com>
2018-10-08 02:30:13 -0400

    day5

---
+++ day5.c
@@ -0,0 +1,36 @@
+/*
+ *					统计字符
+ * 给定一个英文字符串,请写一段代码找出这个字符串中首先出现三次的那个英文字符(需要区分大小写)。
+ * 输入数据一个字符串，包括字母,数字等。输出首先出现三次的那个英文字符
+ * eg:
+ *		输入:Have you ever gone shopping and
+ *		输出:h
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	int std[1000], i;
+	char buf[1024], c;
+	for(i = 0; (c=getchar()) != '\n'; ){
+		buf[i++] = c;
+	}
+	buf[i] = '\0';
+
+	//printf("%s", buf);
+	for(i = 0; buf[i]; i++)
+	{
+		if((buf[i] >= 'a'&& buf[i] <= 'z') || (buf[i] >= 'A' && buf[i] <= 'Z') || (buf[i] >= '0' && buf[i] <= '9'))
+		{
+			std[buf[i]]++;
+			if(std[buf[i]] == 3)
+			{
+				printf("%c\n", buf[i]);
+				break;
+			}
+		}
+	}
+	return 0;
+}
