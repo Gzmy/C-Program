a2cc9346ab2b9a7d21330321d90b932613dd6905
Gzmy <1370711580@qq.com>
2019-01-04 20:33:40 +0800

    正则表达式匹配

---
+++ day80.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class Solution{
+	public:
+		//正则表达式匹配
+		bool match(char* str, char* pattern){
+			if(pattern[0] == 0 && str[0] == 0){
+				return true;
+			}
+
+			if(pattern[0] != 0 && pattern[1] == '*'){
+				if(match(str, pattern+2)){
+					return true;
+				}
+			}
+
+			if((pattern[0] == '.' && str[0]) || str[0] == pattern[0]){
+				if(match(str+1, pattern+1)){
+					return true;
+				}
+				if(pattern[1] == '*' && match(str+1, pattern)){
+					return true;
+				}
+			}
+
+			return false;
+		}
+};
