6c8e46d8239e34ef2cfc7b692a91b8a14b48f6d6
Gzmy <1370711580@qq.com>
2019-05-12 20:08:08 +0800

    LRU algorithm implemented by C++

---
+++ LRU.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <unordered_map>
+
+
+class LRUCache {
+private:
+	struct Node {
+		int _key;
+		int _val;
+		Node(int key, int val) : _key(key), _val(val) {}
+	};
+
+	size_t _capacity;
+	std::list<Node> _cacheList;
+	std::unordered_map<int, std::list<Node>::iterator> _cacheMap;
+
+public:
+	LRUCache(int cap) : _capacity(cap) {}
+
+	int getVal(int key) { // 访问某个页面
+		if(_cacheMap.find(key) == _cacheMap.end()) {
+			return -1; // 产生缺页中断
+		}
+		// 将key移动到第一个 更新_cacheMap
+		_cacheList.splice(_cacheList.begin(), _cacheList, _cacheMap[key]);
+		_cacheMap[key] = _cacheList.begin();
+		return _cacheMap[key]->_val;
+	}
+
+	void setval(int key, int val) {
+		if(_cacheMap.find(key) == _cacheMap.end()) {
+			// 淘汰最后一个
+			if(_cacheList.size() == _capacity) {
+				_cacheMap.erase(_cacheList.back()._key);
+				_cacheList.push_front(Node(key, val));
+				_cacheMap[key] = _cacheList.begin();
+			}
+		} else{
+			// 更新结点即可
+			_cacheMap[key]->_val = val;
+			_cacheList.splice(_cacheList.begin(), _cacheList, _cacheMap[key]);
+			_cacheMap[key] = _cacheList.begin();
+		}
+	}
+};
